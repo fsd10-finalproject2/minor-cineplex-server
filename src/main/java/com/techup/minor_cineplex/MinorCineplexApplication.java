7434b79126e70d2aa24d4d94723709ef2103d6ee
VisneeB <visinee@gmail.com>
2026-03-27 13:35:35 +0700

    Initialize minor-cineplex project with Maven setup and basic application structure

---
+++ src/main/java/com/techup/minor_cineplex/MinorCineplexApplication.java
@@ -0,0 +1,13 @@
+package com.techup.minor_cineplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinorCineplexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinorCineplexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techup/minor_cineplex/MinorCineplexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techup.minor_cineplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinorCineplexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
