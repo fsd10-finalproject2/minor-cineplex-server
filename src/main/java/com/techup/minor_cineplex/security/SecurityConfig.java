b6245f41bef967c16aa5f445ed3d833184829994
fsd10-finalproject2 <fsd10.finalproject2@gmail.com>
2026-04-01 17:29:44 +0700

    Update SecurityConfig.java

--- src/main/java/com/techup/minor_cineplex/security/SecurityConfig.java
+++ src/main/java/com/techup/minor_cineplex/security/SecurityConfig.java
@@ -102,7 +102,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "https://minor-cineplex-app.vercel.app/"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://minor-cineplex-app.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
